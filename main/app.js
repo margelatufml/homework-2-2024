b7dbae75d1b273d819a71c357ee0d59fd7def4ba
Andrei Margelatu <84659334+margelatufml@users.noreply.github.com>
2024-11-25 10:22:22 +0000

    rezolvare_Tema_Margelatu_Andrei

--- main/app.js
+++ main/app.js
@@ -1,11 +1,39 @@
 /**
- * the function renders an object to a tagged string and performs token substitution
- * @param {object} input - a javascript object representing a hierachycal structure  
- * @param {object} values - a list of key value pairs where the key is a token to be replaced with the value in strings present in input
+ * @param {object} input - a javascript object representing a hierarchical structure  
+ * @param {object} values - a list of key-value pairs where the key is a token to be replaced with the value in strings present in input
  */
-function render(input, values){
+function render(input, values) {
+    if (typeof input !== 'object' || input === null || typeof values !== 'object' || values === null) {
+        throw new Error('InvalidType');
+    }
+
+    if (Object.keys(input).length === 0) {
+        return '';
+    }
+
+    function substituteTokens(str) {
+        return str.replace(/\$\{(\w+)\}/g, (match, key) => {
+            return key in values ? values[key] : match;
+        });
+    }
+
+    function renderObject(obj) {
+        if (typeof obj === 'string') {
+            return substituteTokens(obj);
+        }
+
+        if (typeof obj === 'object' && obj !== null) {
+            return Object.entries(obj).map(([key, value]) => {
+                return `<${key}>${renderObject(value)}</${key}>`;
+            }).join('');
+        }
+
+        return '';
+    }
+
+    return renderObject(input);
 }
 
 module.exports = {
     render
-}
+};
